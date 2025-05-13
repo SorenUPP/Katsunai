93097a3bfe7f6c503683b464cc231815142eea46
Soren Beigi <beigiemad@gmail.com>
2025-05-13 22:20:36 +0200

    Delete navbarscroll.js

--- navbarscroll.js
+++
@@ -1,8 +0,0 @@
-const nav = document.querySelector('.navmenu');
-let lastScrollTop = 0;
-
-window.addEventListener('scroll', () => {
-  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  nav.classList.toggle('hidden', scrollTop > lastScrollTop);
-  lastScrollTop = Math.max(scrollTop, 0);
-});
